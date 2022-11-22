cd8d16e039f9953cf5070c806eff3f726622a6b4
ManuelMaia165 <69921343+ManuelMaia165@users.noreply.github.com>
2022-11-22 11:44:17 -0300

    CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.app_context().push()
+CORS(app, supports_credentials=True)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
